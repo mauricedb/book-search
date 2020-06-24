83a29368093518ed919e6885ca9e02c4ae1fc844
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-06-24 11:59:39 +0200

    Bootstrap Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        Book Search
+      </a>
+    </nav>
+  );
+};
+
+Navbar.displayName = "Navbar";
+
+export default Navbar;
